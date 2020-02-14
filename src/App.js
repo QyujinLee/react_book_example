8a16af8ddf3e3c2012927b8326cd9ae1aa9d7488
gyujinLee <gjlee@openit.co.kr>
2020-02-14 14:29:41 +0900

    7장 컴포넌트의 라이프사이클 메서드

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
 import React, { Component } from "react";
 import "./App.css";
-import "./ValidationSample.css";
-// import ValidationSample from "./ValidationSample";
-// import ScrollBox from "./ScrollBox";
-// import Say from "./Say";
-import IterationSample from "./IterationSample";
+import LifeCycleSample from "./LifeCycleSample";
+import ErrorBoundary from "./ErrorBoundary";
+
+function getRandomColor() {
+    return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+    state = {
+        color: "#000000"
+    };
+
+    handleClick = () => {
+        this.setState({
+            color: getRandomColor()
+        });
+    };
     render() {
         return (
             <div>
-                {/* <ValidationSample />
-        <br />
-        <ScrollBox ref={ref => (this.scrollBox = ref)} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
-        <Say /> */}
-                <IterationSample />
+                <button onClick={this.handleClick}>랜덤색상</button>
+                <ErrorBoundary>
+                    <LifeCycleSample color={this.state.color} />
+                </ErrorBoundary>
             </div>
         );
     }
