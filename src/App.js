0b9804c54e828162d28768f580a457fad9f75952
gyujinLee <gjlee@openit.co.kr>
2020-02-14 11:43:55 +0900

    6장 컴포넌트 반복 (배열 데이터 다루기)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import React, { Component } from 'react';
-import './App.css';
-import './ValidationSample.css';
-// import ValidationSample from './ValidationSample';
-import ScrollBox from './ScrollBox';
+import React, { Component } from "react";
+import "./App.css";
+import "./ValidationSample.css";
+// import ValidationSample from "./ValidationSample";
+// import ScrollBox from "./ScrollBox";
+// import Say from "./Say";
+import IterationSample from "./IterationSample";
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-      {/* <ValidationSample/>
-      <br/> */}
-      <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-      <button onClick={() => this.scrollBox.scrollToBottom()}>
-        맨 밑으로
-      </button>
-    </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                {/* <ValidationSample />
+        <br />
+        <ScrollBox ref={ref => (this.scrollBox = ref)} />
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+        <Say /> */}
+                <IterationSample />
+            </div>
+        );
+    }
 }
 
 export default App;
