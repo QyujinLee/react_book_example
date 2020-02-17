3b4b0f26e902a7a8a2d49fd6ca661bc72627a04b
gyujinLee <gjlee@openit.co.kr>
2020-02-17 14:28:27 +0900

    ~ 17.8 정리

--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -1,56 +1,20 @@
-import React from "react";
-import { connect } from "react-redux";
+import React, { useCallback } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
-// import { bindActionCreators } from "redux";
 
-const CounterContainer = ({ number, increase, decrease }) => {
+const CounterContainer = () => {
+    const number = useSelector(state => state.counter.number);
+    const dispatch = useDispatch();
+    const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+    const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
     return (
-        <Counter number={number} onIncrease={increase} onDecrease={decrease} />
+        <Counter
+            number={number}
+            onIncrease={onIncrease}
+            onDecrease={onDecrease}
+        />
     );
 };
 
-// // state값을 props로 전달하기 위함
-// const mapStateToProps = state => ({
-//     number: state.counter.number
-// });
-
-// // 액션 생성함수를 전달하기 위함
-// const mapDispatchToProps = dispatch => ({
-//     increase: () => {
-//         dispatch(increase());
-//     },
-//     decrease: () => {
-//         dispatch(decrease());
-//     }
-// });
-
-// // 두번째 괄호 안에 있는 컴포넌트는 연동할 컴포넌트
-// export default connect(mapStateToProps, mapDispatchToProps)(CounterContainer);
-
-// 위의 로직을 줄인 버젼
-export default connect(
-    state => ({
-        number: state.counter.number
-    }),
-    // dispatch => ({
-    //     increase: () => dispatch(increase()),
-    //     decrease: () => dispatch(decrease())
-    // })
-
-    // // bindActionCreators 유틸 함수로 전하기
-    // dispatch =>
-    //     bindActionCreators(
-    //         {
-    //             increase,
-    //             decrease
-    //         },
-    //         dispatch
-    //     )
-
-    // 함수형태가 아닌 객체 형태로 전하기 (connect 함수가 내부적으로 bindActionCreators 작업을 대신 해줌)
-    {
-        increase,
-        decrease
-    }
-)(CounterContainer);
+export default CounterContainer;
